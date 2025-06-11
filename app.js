b6af8bffeee7a7de94362aee7acc8ae3a8a1dd53
Tilux <tilux001@gmail.com>
2025-06-11 05:53:03 +0100

    Third Commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {createUserWithEmailAndPassword,signInWithEmailAndPassword, sendEmailVerification,updateProfile, GoogleAuthProvider,signInWithPopup} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword, sendEmailVerification,updateProfile, GoogleAuthProvider,signInWithPopup,GithubAuthProvider} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
 import {auth} from "./config.js"
 
 const btn = document.getElementById("btn")
@@ -70,3 +70,43 @@ googleBtn.addEventListener("click", async()=>{
         console.log(error);
     }
 })
+
+
+const provider = new GithubAuthProvider();
+
+document.getElementById("gitHubBtn").addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            const credential = GithubAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken; 
+            localStorage.setItem("TIluxKeep", JSON.stringify(user))
+            if(result.additionalUserInfo?.isNewUser){
+                alert("You're Welcome")
+            }
+            else{
+                alert("Welcome back")
+            }
+            setTimeout(() => {
+                window.location.href = "index.html"
+            }, 1000);
+        })
+        .catch((error) => {
+            const errorMessage = error.code
+            if(errorMessage == "auth/account-exists-with-different-credential"){
+                alert("An account already exists with the same email but different sign-in credentials.")
+            }
+            else if(errorMessage == "auth/popup-blocked"){
+                alert("The popup was blocked by the browser.")
+            }
+            else if(errorMessage == "auth/popup-closed-by-user"){
+                alert("The popup has been closed before authentication completed.")
+            }
+            else if(errorMessage == "auth/invalid-credential"){
+                alert("The supplied auth credential is malformed or expired.")
+            }
+            else if(errorMessage == "auth/network-request-failed"){
+                alert("A network error occurred.")
+            }
+        });
+    });
